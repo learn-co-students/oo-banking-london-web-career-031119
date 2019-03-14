76e7fc660b7dc21dbb3faca45d41ae8717af5b00
github username <github email address>
2019-03-14 13:59:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,41 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+
+    def initialize(name, balance=1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(ammount)
+        @balance += ammount
+    end
+
+    def display_balance
+        return "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @balance >= 1 && @status == "open"
+            return true
+        else
+            return false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
 
 end
+
+
+# def valid?
+#     if @balance >= 1 && @status == "open"
+#         return true
+#     else
+#         return false
+#     end
+# end
